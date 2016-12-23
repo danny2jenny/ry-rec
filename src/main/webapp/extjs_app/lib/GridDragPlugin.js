4a9dbb8701e7e2d1010d2e0ef22474fb4d88fc51
高宏宇 <12793148@qq.com>
2016-12-23 10:33:18 +0800

    GridPanel 用的拖放插件。 1、放置时，触发所属GridPanel 的 onDragDrop 事件 2、拖动时，增加showField属性，用于在拖动时显示的字段名称。

--- src/main/webapp/extjs_app/lib/GridDragPlugin.js
+++ src/main/webapp/extjs_app/lib/GridDragPlugin.js
@@ -2,22 +2,59 @@
  * Created by danny on 16-12-21.
  *
  * 用于拖动的扩展
+ *
+ * 继承至Ext.grid.plugin.DragDrop
+ * 两个子对象
+ * 1、'Ext.view.DragZone'        拖动
+ * 2、'Ext.grid.ViewDropZone'    释放
+ *
+ * 新增属性：
+ * 1 showField 用于显示表格中那个字段用于在拖放时进行显示
  */
 
-Ext.define('ryrec.lib.GridDragPlugin', {
-    extend : 'Ext.grid.plugin.DragDrop',
-    alias : 'plugin.gridDragPlugin',
+Ext.define('app.lib.GridDragPlugin', {
+    extend: 'Ext.grid.plugin.DragDrop',
+    alias: 'plugin.gridDragPlugin',
 
-    onViewRender : function() {
+    onViewRender: function () {
+
+        var me = this;
+
+        // 初始化父类
         this.callParent(arguments);
 
-        //释放后的事件
-        this.dropZone._handleNodeDrop = this.dropZone.handleNodeDrop;
 
-        this.dropZone.handleNodeDrop = function(data, targetNode, position) {
-            //首先进行数据库更新，数据库更新后再进行界面的体现
-            this.view.ownerCt.fireEvent('onDragDrop', data, targetNode, position);
-        };
+        if (this.enableDrop) {
+            //释放后的事件
+            this.dropZone._handleNodeDrop = this.dropZone.handleNodeDrop;
+
+            /**
+             * 调用父控件的事件，所对应的GridPanel的事件。
+             * @param data
+             * @param targetNode
+             * @param position
+             */
+            this.dropZone.handleNodeDrop = function (data, targetNode, position) {
+                //首先进行数据库更新，数据库更新后再进行界面的体现
+                this.view.ownerCt.fireEvent('onDragDrop', data, targetNode, position);
+            };
+        }
+
+
+        if (this.enableDrag) {
+            this.dragZone.getDragText = function () {
+                if (this.dragField) {
+                    var fieldValue = this.dragData.records[0].get(this.dragField);
+                    return Ext.String.format(this.dragText, fieldValue);
+                } else {
+                    //var count = this.dragData.records.length;
+                    //return Ext.String.format(this.dragText, count, count == 1 ? '' : 's');
+                    debugger;
+                    return this.dragData.records[0].data[me.showField];
+                }
+            }
+        }
+
 
     }
 });
